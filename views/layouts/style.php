6e6a0819aa6ea8ccc22981fff8a4c297e34da44a
sangga3331 <1552314@hcmut.edu.vn>
2018-11-16 01:33:03 +0700

    create default.css to custom own css

--- views/layouts/style.php
+++ views/layouts/style.php
@@ -3,4 +3,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<link rel="stylesheet" href="<?php echo URL; ?>public/css/master.css">
+<link rel="stylesheet" href="<?php echo URL; ?>public/css/default.css">
