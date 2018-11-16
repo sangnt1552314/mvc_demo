77549d76c2dfe4de8cc338b31246c422a860e75f
sangga3331 <1552314@hcmut.edu.vn>
2018-11-17 02:29:37 +0700

    confif database

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_TYPE','mysql');
-define('DB_HOST','localhost');
-define('DB_NAME','demo_db');
-define('DB_USER','root');
-define('DB_PASSWORD','');
+define('DB_HOST','us-cdbr-gcp-east-01.cleardb.net');
+define('DB_NAME','gcp_4c7c2d5b2ee7d4d3afcb');
+define('DB_USER','b803bc925ec251');
+define('DB_PASSWORD','b7769d03');
 ?>
