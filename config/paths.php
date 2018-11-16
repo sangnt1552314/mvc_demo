0331e954f08deed15511b8142876861f3fe3b4eb
sangga3331 <1552314@hcmut.edu.vn>
2018-11-17 02:00:56 +0700

    edit url in path

--- config/paths.php
+++ config/paths.php
@@ -1,3 +1,20 @@
 <?php
-define('URL','http://localhost/mvc_demo/');
+$protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos( $_SERVER["SERVER_PROTOCOL"],'/'))).'://';
+$str_test = $_SERVER['REQUEST_URI'];
+$count = 0;
+$find = "/";
+$positions = array();
+for($i = 0; $i<strlen($str_test); $i++)
+{
+     $pos = strpos($str_test, $find, $count);
+     if($pos == $count){
+           $positions[] = $pos;
+     }
+     $count++;
+}
+$length = abs($positions[1] - $positions[0]);
+$request_uri = substr($str_test, $positions[0], $length+1);
+$url = $protocol.$_SERVER['HTTP_HOST'].$request_uri;
+
+define('URL',$url);
 ?>
