e74dc529c1cb98ab38cee101f3a097547d99d8d9
sangga3331 <1552314@hcmut.edu.vn>
2018-11-18 00:45:11 +0700

    edit db pass

--- config/database.php
+++ config/database.php
@@ -3,5 +3,5 @@ define('DB_TYPE','mysql');
 define('DB_HOST','us-cdbr-gcp-east-01.cleardb.net');
 define('DB_NAME','gcp_4c7c2d5b2ee7d4d3afcb');
 define('DB_USER','b803bc925ec251');
-define('DB_PASSWORD','b7769d03');
+define('DB_PASSWORD','dd044906e193d3d');
 ?>
